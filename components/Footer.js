6249ed71888c17da9007318457adf2187e857437
GustavoJurgensen <72266878+GustavoJurgensen@users.noreply.github.com>
2022-03-03 13:17:16 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -29,9 +29,9 @@ export default function Footer(){
                     </div>
                 </div>
                 <div>   
-                    <p>All rights reserved by &copy; 2022  
+                    <p><span>All rights reserved by &copy; 2022</span>  
                     <Link href={"https://github.com/GustavoJurgensen"} to="first" style={{ textDecoration: 'none' ; color: 'whitesmoke'}}>
-                        Gustavo Pilon Jurgensen
+                        <span> Gustavo Pilon Jurgensen</span>
                     </Link>
                     </p>
                 </div>
